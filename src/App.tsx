dcffdf8e27f30d4fa83b35803fbbc83498d78af5
abhishek-udataneni <abshk15@gmail.com>
2021-04-20 23:58:44 +0530

    basic features with out drag and drop completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Column } from './Column';
 import { AddNewItem } from './AddNewItem';
 import { useAppState } from './state/AppStateContext';
 import { addList } from "./state/actions";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend as Backend } from "react-dnd-html5-backend";
 
 export default function App() {
   const { lists, dispatch }= useAppState();
